1fe174e5782fcaa42807d0223ce7634c07685cbd
rohshayy <rohshatahir93@gmail.com>
2026-02-27 12:21:17 +0500

    Add files via upload
    Week 2:
    Guess the Number Game
    Multiplication Table

---
+++ guess_game.py
@@ -0,0 +1,19 @@
+
+import random
+
+number = random.randint(1,10)
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != number and not out_of_guesses:
+    if guess_count < guess_limit:
+        guess = input("Guess a number between 1 and 10: ")
+        guess_count += 1
+    else: out_of_guesses = True
+
+if out_of_guesses:
+    print("Out of guesses - You lose")
+else:
+    print("You win")
